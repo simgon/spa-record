dc72bdee0c088e11b43f0b478cd0bae37f8ac488
simgon <procar1000@yahoo.co.jp>
2024-01-13 09:51:07 +0900

    5段階評価の星を0.5単位で選択できるように変更

--- src/components/SpaList/Star.js
+++ src/components/SpaList/Star.js
@@ -1,6 +1,27 @@
 import React from "react";
-import { FaStar } from "react-icons/fa";
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 
-export default function Star({ selected = false, onSelect = (f) => f }) {
-  return <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />;
+export default function Star({ selected = false, half = false, onSelect = (f) => f }) {
+  const handleStarClick = (event) => {
+    // アイコン幅の左半分がクリックされたかを判定
+    const iconRect = event.target.getBoundingClientRect();
+    const clickX = event.clientX - iconRect.left;
+    const isLeftHalfClicked = clickX < iconRect.width / 2;
+    onSelect(isLeftHalfClicked);
+  };
+
+  return (
+    <>
+      {half ? (
+        // 半分選択時の星（半星）
+        <FaStarHalfAlt color={"red"} onClick={handleStarClick} />
+      ) : selected ? (
+        // 選択時の星（全星）
+        <FaStar color={"red"} onClick={handleStarClick} />
+      ) : (
+        // 未選択時の星（空星）
+        <FaRegStar color={"red"} onClick={handleStarClick} />
+      )}
+    </>
+  );
 }
--- src/components/SpaList/StarRating.js
+++ src/components/SpaList/StarRating.js
@@ -4,8 +4,13 @@ import Star from "./Star";
 export default function StarRating({ totalStars = 5, selectedStars = 0, onRate = (f) => f }) {
   return (
     <>
-      {[...Array(totalStars)].map((n, i) => (
-        <Star key={i} selected={selectedStars > i} onSelect={() => onRate(i + 1)} />
+      {[...Array(totalStars)].map((n, starIdx) => (
+        <Star
+          key={starIdx}
+          selected={selectedStars > starIdx}
+          half={starIdx + 1 - selectedStars == 0.5}
+          onSelect={(half) => onRate(starIdx + 1 - (half ? 0.5 : 0))}
+        />
       ))}
       {/* <p>
         {selectedStars} of {totalStars} stars
